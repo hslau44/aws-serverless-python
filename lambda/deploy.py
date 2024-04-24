02aa9bff6241cded2788bad49ac834131e4d786e
hslau44 <hslau44@gmail.com>
2024-04-24 21:27:06 +0000

    add handlers to be tested

--- lambda/deploy.py
+++ lambda/deploy.py
@@ -1,5 +1,10 @@
 import json
 import logging
+from package.utils import (
+    calculate_length as _calculate_length,
+    get_hello as _get_hello,
+)
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -13,3 +18,25 @@ def handler(event, context):
         },
         'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
     }
+
+
+def hello(event, context):
+    # print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': get_hello()
+    }
+
+
+def calculate(event, context):
+    res = None
+    if not event.get('name'):
+        logger.error("Params does not have 'name'")
+    
+    name = event['name']
+    length = _calculate_length(name=name)
+    response = {'result': f'length of your name:  {length}'}
+    return response
---
+++ lambda/package/utils.py
@@ -0,0 +1,6 @@
+
+def calculate_length(name: str) -> int: 
+    return len(name)
+    
+def get_hello():
+    return "Hello"
