6802387b2d3c3dad368ea1cb50f25c164aa44c82
hslau44 <hslau44@gmail.com>
2024-04-24 21:25:50 +0000

    define lambda and api

--- aws_serverless_python/aws_serverless_python_stack.py
+++ aws_serverless_python/aws_serverless_python_stack.py
@@ -2,6 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
 )
 from constructs import Construct
 
@@ -10,10 +12,22 @@ class AwsServerlessPythonStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        hello_world_function = _lambda.Function(
+            self,
+            "AwsServerlessPythonFunction",
+            runtime = _lambda.Runtime.PYTHON_3_8, # Choose any supported Node.js runtime
+            code = _lambda.Code.from_asset("lambda"), # Points to the lambda directory
+            handler = "deploy.handler", # Points to the 'hello' file in the lambda directory
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsServerlessPythonQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway resource
+        api = apigateway.LambdaRestApi(
+            self,
+            "AwsServerlessPythonApi",
+            handler = hello_world_function,
+            proxy = False,
+        )
+        
+        # Define the '/hello' resource with a GET method
+        hello_resource = api.root.add_resource("page")
+        hello_resource.add_method("GET")
---
+++ lambda/deploy.py
@@ -0,0 +1,15 @@
+import json
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handler(event, context):
+    print('request: {}'.format(json.dumps(event)))
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/plain'
+        },
+        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+    }
